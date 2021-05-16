0f6d76a636a8891b18e83d3f720ae7d262c18ba9
brunobandelli <brunobandelli@gmail.com>
2021-05-15 22:51:45 -0300

    CADASTRANDO DADOS COM API NO POSTMAN

--- index.js
+++ index.js
@@ -26,11 +26,44 @@ var DB = {
     ]
 }
 
-app.get("/", () =>{
-
+app.get("/games", (req, res) =>{
+    res.statusCode = 200
+    res.json(DB.games);
     
 })
 
+app.get("/game/:id",(req, res) =>{
+
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);                //BAD REQUEST
+    }else{
+        var id = parseInt(req.params.id);
+        var game = DB.games.find(g => g.id == id)
+
+        if(game != undefined){
+            res.statusCode = 200;           //REQ FEITA COM SUCESSO
+            res.json(game)
+        }else{
+            res.sendStatus(404);            // NOT FOUND
+        }
+    }
+
+})
+
+app.post("/game",(req, res) =>{
+    
+    var {title, price, year} = req.body;
+
+    DB.games.push({
+        id: 2323,
+        title,
+        price,
+        year
+    });
+
+    res.sendStatus(200);
+
+})
 
 app.listen(45678,() => {
     console.log("API RODANDO!")
